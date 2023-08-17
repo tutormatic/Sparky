5e7e1a108338c4304fc731ed50f5038cd157b24f
tutormatic <137571009+tutormatic@users.noreply.github.com>
2023-08-17 19:15:41 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,11 +1,14 @@
 import subprocess
 import sys
 
-completed_process = subprocess.run(sys.argv[1], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+if sys.argv[2] == "exec":
+    completed_process = subprocess.run(sys.argv[1], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-if completed_process.returncode != 0:
-    print(f"Error: {completed_process.stderr}")
-else:
-    print(completed_process.stdout)
+    if completed_process.returncode != 0:
+        print(f"Error: {completed_process.stderr}")
+    else:
+        print(completed_process.stdout)
     
-    print(completed_process.stderr)
+        print(completed_process.stderr)
+else:
+    # This is for the none selected code
