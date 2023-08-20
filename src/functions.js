15e1487208e710ae7d5688cbc65609842f64d2a9
tutormatic <137571009+tutormatic@users.noreply.github.com>
2023-08-20 13:06:03 -0400

    Add files via upload

---
+++ src/functions.js
@@ -0,0 +1,29 @@
+String.prototype.isString = function() {
+    if (this.startsWith("'") || this.startsWith("\"") && this.endsWith("'") || this.endsWith("\"") || this.startsWith("`") && this.endsWith("`")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+String.prototype.remString = function() {
+    return this.slice(1, - 1);
+}
+
+String.prototype.isNumber = function() {
+    if (/\d/.test(this)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+String.prototype.isVariable = function() {
+    if (!this.isNumber() && !this.isString()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { String };
